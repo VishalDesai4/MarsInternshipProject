0e3909e865fb8accf783b721a8476c92776dc92d
VishalDesai4 <desaivis@gmail.com>
2023-10-03 22:00:42 +1300

    user storyupdated and feature file updated

--- MarsInternship/Pages/LogIn.cs
+++ MarsInternship/Pages/LogIn.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using Octokit;
 using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
@@ -15,7 +16,6 @@ namespace MarsInternship.Pages
             //launch mars portal and navigate to sign in
             driver.Navigate().GoToUrl("http://localhost:5000/");
 
-
             //identify signin button and click button
             IWebElement SignIn = driver.FindElement(By.XPath("//*[@id=\"home\"]/div/div/div[1]/div/a"));
             SignIn.Click();
@@ -27,17 +27,17 @@ namespace MarsInternship.Pages
                 IWebElement usernameTextbox = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[1]/div/div[1]/input"));
                 usernameTextbox.SendKeys("desaivis@gmail.com");
             }
-                
-            catch (Exception)
+
+            catch (Exception ex)
+
             {
-                Assert.Fail("Mars project portal page didnot launch, ex.Message");
+                Assert.Fail("Mars project portal page didnot launch,ex.messsage");
             }
+                
             //Identify password textbox and enter valid password
             IWebElement passwordTextbox = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[1]/div/div[2]/input"));
             passwordTextbox.SendKeys("testmarslogin");
-
-
-
+  
             //identify log in button nand click on the button
             IWebElement loginButton = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[1]/div/div[4]/button"));
             loginButton.Click();
@@ -51,12 +51,9 @@ namespace MarsInternship.Pages
 
         }
 
-
-
-
-
-
-
-
+        internal void LogInActions(object driver)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
