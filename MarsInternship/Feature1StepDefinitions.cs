639c772e3e3e0cf83aedd3febbf9838614e41ab3
VishalDesai4 <desaivis@gmail.com>
2023-10-27 12:00:49 +1300

    user story and coded updated

--- MarsInternship/Feature1StepDefinitions.cs
+++ MarsInternship/Feature1StepDefinitions.cs
@@ -7,7 +7,7 @@ using TechTalk.SpecFlow;
 namespace MarsInternship
 {
     [Binding]
-    public class Feature1StepDefinitions : CommonDriver
+    public class Feature1StepDefinitions
     {
         private IWebDriver driver;
         [Given(@"\[I am on the log in page ]")]
@@ -81,9 +81,8 @@ namespace MarsInternship
         [Given(@"\[\tI navigate to profile page ]")]
         public void GivenINavigateToProfilePage()
         {
-            driver.Navigate().GoToUrl("http://localhost:5000/");
-            IWebElement SignIn = driver.FindElement(By.XPath("//*[@id=\"home\"]/div/div/div[1]/div/a"));
-            SignIn.Click();
+            IWebElement profilepage = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[1]/div/a[2]"));
+            profilepage.Click();
         }
 
         [When(@"\[Click on add new button]")]
@@ -107,9 +106,8 @@ namespace MarsInternship
         [Given(@"\[\tI am on profile page ]")]
         public void GivenIAmOnProfilePage()
         {
-            driver.Navigate().GoToUrl("http://localhost:5000/");
-            IWebElement SignIn = driver.FindElement(By.XPath("//*[@id=\"home\"]/div/div/div[1]/div/a"));
-            SignIn.Click();
+            IWebElement profilepage = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[1]/div/a[2]"));
+            profilepage.Click();
         }
 
         [Given(@"\[Click on add new button]")]
@@ -145,79 +143,111 @@ namespace MarsInternship
         [Then(@"\[I should be able to see raw for language has been added]")]
         public void ThenIShouldBeAbleToSeeRawForLanguageHasBeenAdded(Table table)
         {
+           
             
         }
+
+
+
 
         [Given(@"\[I will navigate to profile page ]")]
         public void GivenIWillNavigateToProfilePage()
         {
-            throw new PendingStepException();
+            IWebElement profilepage = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[1]/div/a[2]"));
+            profilepage.Click();
         }
 
         [When(@"\[click delet button]")]
         public void WhenClickDeletButton()
         {
-            throw new PendingStepException();
+            IWebElement Delete = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody/tr/td[3]/span[2]/i"));
+            Delete.Click();
         }
 
         [Then(@"\[I should be able to message for language has bee deleted]")]
         public void ThenIShouldBeAbleToMessageForLanguageHasBeeDeleted()
         {
-            throw new PendingStepException();
+            
         }
+
+
+
 
         [Given(@"\[Click on Edit record and Cancel record]")]
         public void GivenClickOnEditRecordAndCancelRecord()
         {
-            throw new PendingStepException();
+            IWebElement Edit = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody[1]/tr/td[3]/span[1]/i"));
+            Edit.Click();
+            IWebElement CancelEdit = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody[1]/tr/td/div/span/input[2]"));
+            CancelEdit.Click();
+
+
         }
 
         [When(@"\[Edit rlanguage record and change level of proficiency]")]
         public void WhenEditRlanguageRecordAndChangeLevelOfProficiency()
         {
-            throw new PendingStepException();
+            IWebElement Edit = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody[1]/tr/td[3]/span[1]/i"));
+            Edit.Click();
+            IWebElement ClickPullDownMenu = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/div/div[2]/select"));
+            ClickPullDownMenu.Click();
+            IWebElement ChangeLevel = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody[1]/tr/td/div/div[2]/select"));
+            ChangeLevel.Click();
+
         }
 
         [Then(@"\[I should be able to upate record]")]
         public void ThenIShouldBeAbleToUpateRecord()
         {
-            throw new PendingStepException();
+           
         }
+
+
+
 
         [Given(@"\[\tI will click on profile page ]")]
         public void GivenIWillClickOnProfilePage()
         {
-            throw new PendingStepException();
+            IWebElement profilepage = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[1]/div/a[2]"));
+            profilepage.Click();
         }
 
         [Given(@"\[click on ""([^""]*)""]")]
         public void GivenClickOn(string p0)
         {
-            throw new PendingStepException();
+            IWebElement ClickOnSkillButton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[1]/a[2]"));
+            ClickOnSkillButton.Click();
+            IWebElement AddNewButtonSkill = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/thead/tr/th[3]/div"));
+            AddNewButtonSkill.Click();
         }
 
         [Given(@"\[write First skill that you have to share it]")]
         public void GivenWriteFirstSkillThatYouHaveToShareIt()
         {
-            throw new PendingStepException();
+            IWebElement AddSkill = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/div/div[1]/input"));
+            AddSkill.Click();
+            
         }
 
         [Given(@"\[ click pull down menu]")]
         public void GivenClickPullDownMenu()
         {
-            throw new PendingStepException();
+            IWebElement ClickOnPullDown = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/div/div[2]/select"));
+            ClickOnPullDown.Click();
         }
 
         [Given(@"\[select level of skill]")]
         public void GivenSelectLevelOfSkill()
         {
-            throw new PendingStepException();
+            IWebElement SelectLevel = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/div/div[2]/select"));
+            SelectLevel.Click();
         }
 
         [When(@"\[click add button]")]
         public void WhenClickAddButton()
         {
-           
+            IWebElement addskillraw = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/div/span/input[1]"));
+            addskillraw.Click();
         }
 
         [Then(@"\[I should be able to see skill that has been added]")]
@@ -226,34 +256,50 @@ namespace MarsInternship
             
         }
 
+
+
+
         [When(@"\[Click on add new  skill button]")]
         public void WhenClickOnAddNewSkillButton()
         {
-            
+            IWebElement AddNewButtonSkill = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/thead/tr/th[3]/div"));
+            AddNewButtonSkill.Click();
         }
 
         [Then(@"\[Click on cancel button to cancel it]")]
         public void ThenClickOnCancelButtonToCancelIt()
         {
-            
+            IWebElement cancelSkill = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/div/span/input[2]"));
+            cancelSkill.Click();
         }
+
+
 
         [Given(@"\[I will be navigate to profile page ]")]
         public void GivenIWillBeNavigateToProfilePage()
         {
-           
+            IWebElement profilepage = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[1]/div/a[2]"));
+            profilepage.Click();
         }
 
         [Given(@"\[Click on Edit button and Cancel record by cliking cancel]")]
         public void GivenClickOnEditButtonAndCancelRecordByClikingCancel()
         {
-            
+            IWebElement skilledit = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[2]/tr/td[3]/span[1]/i"));
+            skilledit.Click();
+            IWebElement skilleditcancel = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[2]/tr/td[3]/span[2]/i"));
+            skilleditcancel.Click();    
         }
 
         [When(@"\[Edit skill record and change level of  skill proficiency]")]
         public void WhenEditSkillRecordAndChangeLevelOfSkillProficiency()
         {
-            
+            IWebElement skilledit = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[2]/tr/td[3]/span[1]/i"));
+            skilledit.Click();
+            IWebElement Clickpullbutton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[2]/tr/td/div/div[2]/select"));
+            Clickpullbutton.Click();
+            IWebElement changeskilllevel = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[2]/tr/td/div/div[2]/select"));
+            changeskilllevel.Click();   
         }
 
         [Then(@"\[I should be able to upate skill record]")]
@@ -268,13 +314,15 @@ namespace MarsInternship
         [Given(@"\[I will naviagte to skill page ]")]
         public void GivenIWillNaviagteToSkillPage()
         {
-           
+            IWebElement AddNewButtonSkill = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/thead/tr/th[3]/div"));
+            AddNewButtonSkill.Click();
         }
 
         [When(@"\[click delete button on skill]")]
         public void WhenClickDeleteButtonOnSkill()
         {
-            
+            IWebElement deleteSkill = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody[2]/tr/td[3]/span[2]"));
+            deleteSkill.Click();    
         }
 
         [Then(@"\[I should be able delete skill raw]")]
